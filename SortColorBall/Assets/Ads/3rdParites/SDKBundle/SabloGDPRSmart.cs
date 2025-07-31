60e59d3cc450f4ac787911ef7318710bcb7b0927
Nadeem <71691410+fojiphoto@users.noreply.github.com>
2025-07-31 13:05:04 +0500

    Ready

--- SortColorBall/Assets/Ads/3rdParites/SDKBundle/SabloGDPRSmart.cs
+++ SortColorBall/Assets/Ads/3rdParites/SDKBundle/SabloGDPRSmart.cs
@@ -45,6 +45,6 @@ public class SabloGDPRSmart : MonoBehaviour
     
     public void OpenPrivacyLink()
     {
-        Application.OpenURL("https://zvconcepts.com/privacy-policy/");
+        Application.OpenURL("https://factorialstudio.blogspot.com/2023/01/privacy-policy.html");
     }
 }
